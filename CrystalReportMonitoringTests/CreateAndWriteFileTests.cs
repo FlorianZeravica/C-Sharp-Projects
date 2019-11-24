a1de13884314b85d9682ccc3640ea0721f275026
Florian Zeravica <zeravica.florian@web.de>
2019-11-24 19:35:01 +0100

    CrystalReportMonitoringToolTests

---
+++ CrystalReportMonitoringTests/CreateAndWriteFileTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using CrystalReportMonitoring;
+
+namespace CrystalReportMonitoringTests
+{
+    internal sealed class FileWriterTests
+    {
+        [TestCase("CrystalReportsMonitoringFile", "", false)]
+        [TestCase("", "CrystalReportsMonitoringFile1", false)]
+        [TestCase("CrystalReportsMonitoringFile", "CrystalReportsMonitoringFile2", true)]
+        [TestCase("CrystalReportsMonitoringFile", "CrystalReportsMonitoringFile3", false)]
+        [TestCase("CrystalReportsMonitoringFile1", "CrystalReportsMonitoringFile2", false)]
+        [TestCase("CrystalReportsMonitoringFile1", "CrystalReportsMonitoringFile4", false)]
+
+        public void CreateAndWriteFile(string defaultValue, string comparingValue, bool expectedResult)
+        {
+            // Arrange
+            var rule = new FileWriter();
+
+            // Act
+            var result = rule.CreateAndWriteFile(null, defaultValue, comparingValue, null);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ CrystalReportMonitoringTests/GetSubdirectoriesTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using CrystalReportMonitoring;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CrystalReportMonitoringTests
+{
+    internal sealed class GetSubdirectoriesTests
+    {
+        [TestCase(".")]
+        public void GetSubdirectories(string defaultValue)
+        {
+            // Arrange
+            var rule = new Engine();
+                        
+            // Act
+            List<string> result = new List<string>();
+            result.AddRange(rule.GetSubdirectories(defaultValue));
+            
+            Assert.GreaterOrEqual(result.Count, 1);         
+        }
+
+        [TestCase(".")]
+        public void TestExcludingDirectory(string defaultValue)
+        {
+            // Arrange
+            var rule = new Engine();
+
+            // create dummy
+            string testFolderPath = string.Format(".\\{0}", Engine.ExcludingDirectory);
+            System.IO.Directory.CreateDirectory(testFolderPath);
+
+            // Act
+            List<string> result = new List<string>();
+            result.AddRange(rule.GetSubdirectories(defaultValue));
+
+            Assert.That(result.Where(item => item.Contains(Engine.ExcludingDirectory)).Count() <= 0);
+
+            System.IO.Directory.Delete(".\\{0}");
+        }
+    }
+}
